bca4702e1c78aafc08eb5d9bbd7b649928e56dd2
Aaron Landwehr <snaphat@gmail.com>
2022-04-13 22:29:46 -0400

    Update patcher to use ILRepack and to work with modern dnlib.

--- auto_patcher.cs
+++ auto_patcher.cs
@@ -7,7 +7,7 @@ using System.Text;
 class Program
 {
     static StringBuilder errorBuffer = new StringBuilder();
-    
+
     public static bool IsDelegate(dynamic typeDefinition)
     {
         if (typeDefinition.BaseType == null)
@@ -50,6 +50,18 @@ class Program
         //check if the file exists.
         if (File.Exists(filename) == false)
         {
+
+            var values = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, filename);
+                if (File.Exists(fullPath))
+                {
+                    // Success (in path)
+                    return true;
+                }
+            }
+
             //write error.
             errorBuffer.Append("Program '" + filename + "' does not exist.");
 
@@ -151,7 +163,7 @@ class Program
                     String backupFile = filename + ".bak";
                     File.Copy(filename, backupFile);
                 }
-                
+
                 byte[] dataFile = System.IO.File.ReadAllBytes(filename);
 
                 //load the dnlib assembly.
@@ -159,13 +171,14 @@ class Program
 
                 //Grab the dnlib.DotNet.ModuleDefMD type.
                 Type moduleDefinitionType = assembly.GetType("dnlib.DotNet.ModuleDefMD");
+                Type ModuleCreationOptionsType = assembly.GetType("dnlib.DotNet.ModuleCreationOptions");
 
-                //Get the dnlib.DotNet.ModuleDefMD.Load(byte[]) method.
-                Type[] argumentTypes = { typeof(byte[]) };
+                //Get the dnlib.DotNet.ModuleDefMD.Load(byte[], ModuleCreationOptions) method.
+                Type[] argumentTypes = { typeof(byte[]), ModuleCreationOptionsType };
                 MethodInfo methodInfo = moduleDefinitionType.GetMethod("Load", argumentTypes);
 
-                //Call the dnlib.DotNet.ModuleDefMD.Load(byte[]) method. Note: it is static, first argument is null as a result.
-                Object[] arguments = { dataFile };
+                //Call the dnlib.DotNet.ModuleDefMD.Load(byte[], ModuleCreationOptions) method. Note: it is static, first argument is null as a result.
+                Object[] arguments = { dataFile, null };
                 dynamic module = methodInfo.Invoke(null, arguments);
 
                 //Loop through each class.
@@ -189,19 +202,19 @@ class Program
                                 type.Attributes |= Convert.ChangeType(Enum.Parse(classAttributeType, "Public"), classAttributeType);
                                 type.Attributes &= ~Convert.ChangeType(Enum.Parse(classAttributeType, "NotPublic"), classAttributeType);
                             }
-                            
+
                             //unseal the class.
                             if(type.IsAbstract == false)
                             {
                                 type.Attributes &= ~Convert.ChangeType(Enum.Parse(classAttributeType, "Sealed"), classAttributeType);
                             }
                         }
-                        
+
                         //Loop through each field.
                         dynamic fieldAttributeType = assembly.GetType("dnlib.DotNet.FieldAttributes");
                         foreach (dynamic field in type.Fields)
                         {
-                            //Make each field public. 
+                            //Make each field public.
                             field.Attributes |= Convert.ChangeType(Enum.Parse(fieldAttributeType, "Public"), fieldAttributeType);
                             field.Attributes &= ~Convert.ChangeType(Enum.Parse(fieldAttributeType, "Private"), fieldAttributeType);
                         }
@@ -213,7 +226,7 @@ class Program
                             //Make each method public.
                             method.Attributes |= Convert.ChangeType(Enum.Parse(methodAttributeType, "Public"), methodAttributeType);
                             method.Attributes &= ~Convert.ChangeType(Enum.Parse(methodAttributeType, "Private"), methodAttributeType);
-                            
+
                             //Make each method virtual if it isn't static/constructor/abstract
                             if(method.IsStatic == false  && method.IsAbstract == false && method.IsConstructor == false)
                             {
@@ -226,7 +239,7 @@ class Program
 
                 //Write out the module.
                 module.Write(filename);
-                
+
                 return 0;
             }
         }
@@ -252,8 +265,8 @@ class Program
 
         //Merge de4dot with all of the assemblies it depends on into an assembly called de4dotp.exe.
         Console.WriteLine("Merging '"+ origFile +"' and DLLs into a packed executable '"+ modFile +"'...\r\n");
-        String ilmergeProgram = GetProgramFilesx86() + "\\Microsoft\\ILMerge\\ILMerge.exe";
-        String arguments = origFile + " bin\\AssemblyData.dll bin\\AssemblyServer.exe bin\\de4dot.blocks.dll bin\\de4dot.code.dll bin\\de4dot.cui.dll bin\\de4dot.mdecrypt.dll bin\\dnlib.dll /out:" + modFile;
+        String ilmergeProgram = "ILRepack.exe";
+        String arguments = ".\\"+origFile + " AssemblyData.dll AssemblyServer.exe de4dot.blocks.dll de4dot.code.dll de4dot.cui.dll de4dot.mdecrypt.dll dnlib.dll /out:" + modFile;
         returnValue = RunProgram(ilmergeProgram, arguments);
 
         if (returnValue == 0)
@@ -266,7 +279,7 @@ class Program
 
             //Patch de4dotp.exe to allow public access to all classes, fields, and methods and virtualizing methods.
             Console.WriteLine("Patching '"+ modFile +"', (1) Publicizing classes/fields/methods, (2) unsealing classes, (3) virtualizing methods...\r\n");
-            returnValue = DoPatch(modFile, "bin\\dnlib.dll", false);
+            returnValue = DoPatch(".\\"+modFile, "dnlib.dll", false);
         }
 
         //print error message
